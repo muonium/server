d07c575b686ae57d5c5ac3a25e1fe6813db661a7
hitoshi54 <dylanclement7@gmail.com>
2018-03-27 00:04:42 +0200

    unshare a file: pass file id in url due to delete method

--- application/controllers/share.php
+++ application/controllers/share.php
@@ -10,6 +10,26 @@ class share extends c\FileManager {
     function __construct() {
         parent::__construct();
     }
+	
+	public function idAction($id) {
+		header("Content-type: application/json");
+		$resp = self::RESP;
+		$method = h\httpMethodsData::getMethod();
+		$data = h\httpMethodsData::getValues();
+		$data->id = $id;
+		$resp['token'] = $this->_token;
+		
+		if($method === 'post') {
+			$resp = $this->post($resp, $data);
+		} elseif($method === 'delete') {
+			$resp = $this->delete($resp, $data);
+		} else {
+			$resp['code'] = 405; // Method Not Allowed
+		}
+		
+		http_response_code($resp['code']);
+		echo json_encode($resp);
+	}
 
 	public function DefaultAction() {
 		header("Content-type: application/json");
@@ -19,31 +39,39 @@ class share extends c\FileManager {
 		$resp['token'] = $this->_token;
 
 		if($method === 'post') {
-			if(isset($data->id) && is_pos_digit($data->id) && isset($data->dk)) {
-				$this->_modelFiles = new m\Files($this->_uid);
-				if($this->_modelFiles->setDK(intval($data->id), $data->dk)) {
-					$resp['code'] = 200;
-					$resp['status'] = 'success';
-					$resp['data'] = URL_APP.'/dl/?'.setURL(intval($data->id));
-				}
-			} else {
-				$resp['message'] = 'emptyField';
-			}
-		} elseif($method === 'delete') {
-			if(isset($data->id) && is_pos_digit($data->id)) {
-				$this->_modelFiles = new m\Files($this->_uid);
-				if($this->_modelFiles->setDK(intval($data->id), null)) {
-					$resp['code'] = 200;
-					$resp['status'] = 'success';
-				}
-			} else {
-				$resp['message'] = 'emptyField';
-			}
+			$resp = $this->post($resp, $data);
 		} else {
 			$resp['code'] = 405; // Method Not Allowed
 		}
 
 		http_response_code($resp['code']);
 		echo json_encode($resp);
+	}
+	
+	private function post($resp, $data) {
+		if(isset($data->id) && is_pos_digit($data->id) && isset($data->dk)) {
+			$this->_modelFiles = new m\Files($this->_uid);
+			if($this->_modelFiles->setDK(intval($data->id), $data->dk)) {
+				$resp['code'] = 200;
+				$resp['status'] = 'success';
+				$resp['data'] = URL_APP.'/dl/?'.setURL(intval($data->id));
+			}
+		} else {
+			$resp['message'] = 'emptyField';
+		}
+		return $resp;
+	}
+	
+	private function delete($resp, $data) {
+		if(isset($data->id) && is_pos_digit($data->id)) {
+			$this->_modelFiles = new m\Files($this->_uid);
+			if($this->_modelFiles->setDK(intval($data->id), null)) {
+				$resp['code'] = 200;
+				$resp['status'] = 'success';
+			}
+		} else {
+			$resp['message'] = 'emptyField';
+		}
+		return $resp;
 	}
 }
