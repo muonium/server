13935724739580fbbfeeab3eb4ba62a29d1650fb
dclement8 <dylanclement7@protonmail.ch>
2018-05-22 22:51:38 +0200

    update composer

--- application/controllers/GoogleAuthenticator.php
+++ application/controllers/GoogleAuthenticator.php
@@ -4,18 +4,18 @@ namespace application\controllers;
 
 use \library as h;
 use \library\MVC as l;
-use \library\GA as ga;
+use \Muonium\GoogleAuthenticator as ga;
 use \application\models as m;
 use \config as conf;
 
 class GoogleAuthenticator extends l\Controller {
 
     private $_forbiddenChars = ["0", "1", "8", "9"];
-    
+
     function __construct() {
         parent::__construct();
     }
-    
+
     public function DefaultAction() {
 		header("Content-type: application/json");
 		$resp = self::RESP;
@@ -24,7 +24,7 @@ class GoogleAuthenticator extends l\Controller {
 		http_response_code($resp['code']);
 		echo json_encode($resp);
 	}
-    
+
     public function generateQRcodeAction() {
         header("Content-type: application/json");
 		$resp = self::RESP;
@@ -36,25 +36,25 @@ class GoogleAuthenticator extends l\Controller {
 		}
         elseif(isset($data->username)) {
             $googleAuth = new ga\GoogleAuthenticator();
-            
+
             $secret = $data->username.conf\confGoogleAuthenticator::salt;
             $secret = (new ga\FixedBitNotation(5, 'ABCDEFGHIJKLMNOPQRSTUVWXYZ234567', true, true))->encode($secret);
-            
+
             $url = ga\GoogleQrUrl::generate($data->username, $secret, 'Muonium');
-            
+
             $ch = curl_init();
             curl_setopt($ch, CURLOPT_URL, $url);
             curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
             curl_setopt($ch, CURLOPT_USERAGENT, 'Muonium');
             $result = curl_exec($ch);
             curl_close($ch);
-            
-            
+
+
             $resp['code'] = 200;
             $resp['status'] = 'success';
             $resp['data']['QRcode'] = base64_encode($result);
             $resp['data']['secretKey'] = $secret;
-            
+
         } else {
 			$resp['message'] = 'emptyField';
 		}
